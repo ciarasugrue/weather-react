3e429e237e4fb79bb106a0e55575d5059eea5690
ciarasugrue <86407704+ciarasugrue@users.noreply.github.com>
2021-09-16 10:10:49 +0100

    tidy up index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import Coder from './Coder';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
